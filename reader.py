0946b1265ca903abb7abb6fe3c5e0d9891f19fa0
Greg-hillan123 <87268385+Greg-hillan123@users.noreply.github.com>
2024-01-25 15:41:34 +0000

    Update reader.py
    Button code included

--- reader.py
+++ reader.py
@@ -2,11 +2,12 @@ import smbus2
 import bme280
 import time
 from rpi_lcd import LCD
+from gpiozero import Button
+button = Button(20)
 
+counter = 0
 #instantiate
 lcd = LCD()
-
-#Specify address of sensor
 address = 0x77
 
 #bus address 4 is used for our sensor
@@ -18,18 +19,24 @@ running = True
 
 while running == True:
         try:
-                data = bme280.sample(bus,address2,calibration_params)
-                print(data.timestamp)
-                print(data.temperature)
-                print(data.pressure)
-                print(data.humidity)
-
+                data = bme280.sample(bus,address,calibration_params)
                 rounded_temp = round(data.temperature, 2)
                 rounded_hum = round(data.humidity, 2)
                 rounded_pres = round(data.pressure, 2)
-                lcd.text("Tem: " + str(rounded_temp) + "Hum: " + str(rounded_hum) + "% " + "Kpa " + str(rounded_pres), 1)
-                time.sleep(3)
+                options =[rounded_temp, rounded_hum, rounded_pres]
+                if counter == 0:
+                    lcd.text(str(options[counter]), 1)
+                    button.wait_for_press()
+                    counter += 1
+                elif counter == 1:
+                    lcd.text(str(options[counter]), 1)
+                    button.wait_for_press()
+                    counter += 1
+                elif counter == 2:
+                    lcd.text(str(options[counter]), 1)
+                    button.wait_for_press()
+                    counter = 0
         except KeyboardInterrupt:
                 running = False
 
-#These variables could easily be written to a csv file to allow for recording of the data and possibly machine learning tasks
+#These variables could easily be written to a CSV file to allow for the recording of the data and possibly machine learning tasks
